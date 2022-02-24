01e719570a9d6bd3d6cc68a6e8902ca725085def
Alex Di Guida <alexandre.diguida@gmail.com>
2022-02-24 15:50:32 +0000

    Update css

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,10 +1,10 @@
 export const hideContainer = () => {
-  const container = document.querySelector('.container')
+  const container = document.querySelector('.permissions-container')
   container.style.display = 'none'
 }
 
 export const showData = () => {
-  const container = document.querySelector('.data-container')
+  const container = document.querySelector('.debug-container')
   container.style.display = 'flex'
 }
 

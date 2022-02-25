c283308f0cb524bd7b19960ad2d7f6d590ad48b3
Alex Di Guida <alexandre.diguida@gmail.com>
2022-02-25 09:38:33 +0000

    Update options watch position

--- src/js/deviceGeolocation.js
+++ src/js/deviceGeolocation.js
@@ -1,12 +1,17 @@
 import LocalCoordSystem from './localCoordSystem'
 
-const FREQUENCY = 1000 // ms
+const DEFAULT_F = 1000 // ms
 
 class Device_Geo_Location {
   init({ createWorldObjects, camera, updateWorld }) {
     this.updateWorld = updateWorld
     this.createWorldObjects = createWorldObjects
     this.camera = camera
+
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const fParams = urlParams.get('f')
+    this.frequency = fParams ? Number(fParams) : DEFAULT_F
 
     // Dom
     this.speedDom = document.getElementById('val1')
@@ -19,7 +24,11 @@ class Device_Geo_Location {
     this.geoLoc = navigator.geolocation
     this.options = {
       enableHighAccuracy: true,
+      timeout: this.frequency - 100,
+      maximumAge: 0,
     }
+
+    console.log('Device_Geo_Location', this.options)
 
     this.bind()
     this.updatePosition()
@@ -71,58 +80,15 @@ class Device_Geo_Location {
     }
 
     this.domUpdate()
-
-    // Only trigger timeout when the previous one is done
-    // if (!this.isWorking) {
-    //   if (this.timeout) clearTimeout(this.timeout)
-
-    //   this.isWorking = true
-
-    //   this.timeout = setTimeout(() => {
-    //     console.log('watchPositionUpdate > update every 500ms')
-    //     this.speed = speed
-
-    //     // First time: store initial coordiantes
-    //     if (!this.initCoords) {
-    //       this.initCoords = {
-    //         longitude,
-    //         latitude,
-    //       }
-
-    //       LocalCoordSystem.setOrigin(latitude, longitude)
-    //       this.createWorldObjects()
-    //       this.isInit = true
-    //     }
-
-    //     // Once setup done: keep updating the camera position
-    //     if (this.isInit) {
-    //       LocalCoordSystem.getPosition(
-    //         this.camera.position,
-    //         latitude,
-    //         longitude
-    //       )
-    //       this.updateWorld()
-    //     }
-
-    //     this.domUpdate()
-
-    //     this.isWorking = false
-    //   }, FREQUENCY)
-    // }
   }
 
   updatePosition() {
-    // this.watchID = this.geoLoc.getCurrentPosition(
-    //   this.watchPositionUpdate,
-    //   this.watchPositionError,
-    //   this.options
-    // )
-    const queryString = window.location.search
-    const urlParams = new URLSearchParams(queryString)
-    const fParams = urlParams.get('f')
-    const frequency = fParams ? Number(fParams) : FREQUENCY
+    const frequency = this.frequency
 
-    console.log({ frequency })
+    console.log('debug - updatePosition()', {
+      frequency,
+      options: this.options,
+    })
 
     this.interval = setInterval(() => {
       this.watchID = this.geoLoc.getCurrentPosition(
@@ -134,6 +100,7 @@ class Device_Geo_Location {
   }
 
   bind() {
+    this.updatePosition = this.updatePosition.bind(this)
     this.watchPositionUpdate = this.watchPositionUpdate.bind(this)
     this.watchPositionError = this.watchPositionError.bind(this)
     this.domUpdate = this.domUpdate.bind(this)
@@ -141,13 +108,10 @@ class Device_Geo_Location {
 
   dispose() {
     if (this.geoLoc) this.geoLoc.clearWatch(this.watchID)
-    if (this.timeout) clearTimeout(this.timeout)
-    // clearInterval(this.interval)
+    if (this.interval) clearInterval(this.interval)
   }
 
-  update() {
-    // this.updatePosition()
-  }
+  update() {}
 }
 
 const DeviceGeoLocation = new Device_Geo_Location()
